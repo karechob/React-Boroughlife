d44c7b059a007eea66953d265a9d248245b2f020
imaesp <127232789+imaesp@users.noreply.github.com>
2024-05-27 23:54:56 -0400

    updated NewsFeed component

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -2,40 +2,42 @@ import Carousel from "react-bootstrap/Carousel";
 
 function Carrousel() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          style={{ objectFit: "contain" }}
-          src="src\assets\8cf2de649147e8f3f705d07d0e2c2e62.png"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ objectFit: "contain" }}
-          src="src\assets\manhattan-usa-us-united.jpg"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          style={{ objectFit: "contain" }}
-          src="src\assets\wallpapersden.com_sunrise-over-lower-manhattan_1920x1080.jpg"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            style={{ objectFit: "contain" }}
+            src="src\assets\8cf2de649147e8f3f705d07d0e2c2e62.png"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ objectFit: "contain" }}
+            src="src\assets\manhattan-usa-us-united.jpg"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            style={{ objectFit: "contain" }}
+            src="src\assets\wallpapersden.com_sunrise-over-lower-manhattan_1920x1080.jpg"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
--- src/components/NewsFeed.tsx
+++ src/components/NewsFeed.tsx
@@ -5,7 +5,7 @@ function NewsFeed() {
     <div>
         <div>
             <div>
-                <ul>
+                <ul  style={{marginLeft: '400px', marginTop: '50px'}}>
                     <li style={{display: 'inline-block'}}>
                         <div>
                             <h1 style={{font: 'Times-Roman', color: '#EB3B15', fontWeight: 'bolder', fontSize: 30}}>
@@ -13,6 +13,22 @@ function NewsFeed() {
                                 <img style={{height: 400, width: 'auto'}} src="src\assets\8cf2de649147e8f3f705d07d0e2c2e62.png"/>
                             </h1>
                         </div>
+                        <div style={{maxWidth: '650px'}}>
+                            <p>
+                            Sample Text Sample Text Sample Text Sample TextSample Text
+                                Sample Text Sample Text Sample Text Sample Text Sample Text
+                                Sample Text Sample Text Sample Text Sample Text 
+                            </p>
+                        </div>
+                        <div style={{maxWidth: '350px'}}>
+                            <img style={{height: 200, width: 'auto', display: 'inline-block'}} src="src\assets\NYC-Social-Share-Image-iStock-985069044.jpg" />
+                            <p style={{display: 'inline-block'}}>
+                                Sample Text Sample Text Sample Text Sample TextSample Text
+                                Sample Text Sample Text Sample Text Sample Text Sample Text
+                                Sample Text Sample Text Sample Text Sample Text  
+                            </p>
+                        </div>
+         
                     </li>
                     <li style={{display: 'inline-block'}}>
                         <div>
@@ -21,14 +37,14 @@ function NewsFeed() {
                             </h2>
                         </div>
                         <div>
-                            <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30}}>
+                            <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30, maxWidth: '350px'}}>
                                 Sample Text Sample Text Sample Text Sample TextSample Text
                                 Sample Text Sample Text Sample Text Sample Text Sample Text
                                 Sample Text Sample Text Sample Text Sample Text  
                             </p>
                         </div>
                         <div>
-                            <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30}}>
+                            <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30, maxWidth: '350px'}}>
                                 Sample Text Sample Text Sample Text Sample TextSample Text
                                 Sample Text Sample Text Sample Text Sample Text Sample Text
                                 Sample Text Sample Text Sample Text Sample Text  
