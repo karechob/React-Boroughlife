c9343f18311c52bdd51b92ffad925a56bd00a5f9
imaesp <127232789+imaesp@users.noreply.github.com>
2024-05-28 15:01:47 -0400

    Updated NewsFeed and created NewsItem Components
    Installed axios to manage get requests from NewsAPI

--- src/components/NewsFeed.tsx
+++ src/components/NewsFeed.tsx
@@ -1,69 +1,92 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewsItem from "./NewsItem";
 
-function NewsFeed() {
+const NewsFeed = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=99f062a792664e49814dd86156b78dd0"
+      );
+      console.log(response);
+      setArticles(response.data.articles);
+    };
+    getArticles();
+  }, []);
   return (
     <div>
-        <div>
-            <div style={{marginLeft: '350px', marginTop: '50px'}}>
-                <ul style={{display: 'flex'}}>
-                    <li>
-                        <div>
-                            <h1 style={{font: 'Times-Roman', color: '#EB3B15', fontWeight: 'bolder', fontSize: 30}}>
-                                LATEST NEWS
-                                <img style={{height: 'auto', width: '750px'}} src="src\assets\8cf2de649147e8f3f705d07d0e2c2e62.png"/>
-                            </h1>
-                        </div>
-                        <div style={{width: '750px'}}>
-                            <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 30, marginTop: '30px'}}>
-                                Title Sample
-                            </p>
-                            <p>
-                                Sample Text Sample Text Sample Text Sample TextSample Text
-                                Sample Text Sample Text Sample Text Sample Text Sample Text
-                                Sample Text Sample Text Sample Text Sample Text 
-                            </p>
-                        </div>
-                        <div style={{maxWidth: '650px', display: 'flex', marginTop: '30px'}}>
-                            <img style={{height: 200, width: 'auto'}} src="src\assets\NYC-Social-Share-Image-iStock-985069044.jpg" />
-                            <div style={{marginLeft: '20px'}}>
-                                <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 30, display: 'grid'}}>
-                                    Title Sample
-                                </p>
-                                <p>
-                                    Sample Text Sample Text Sample Text Sample TextSample Text
-                                </p>
-                            </div>
-                        </div>
-         
-                    </li>
-                    <li>
-                        <div style={{marginLeft: '30px'}}>
-                            <div style={{marginBottom: '10px'}}>
-                                <h2 style={{font: 'Times-Roman', color: '#EB3B15', fontWeight: 'bolder', fontSize: 30}}>
-                                    POPULAR NEWS
-                                </h2>
-                            </div>
-                            <div>
-                                <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30, maxWidth: '450px'}}>
-                                    Sample Text Sample Text Sample Text Sample TextSample Text
-                                    Sample Text Sample Text Sample Text Sample Text Sample Text
-                                    Sample Text Sample Text Sample Text Sample Text  
-                                </p>
-                            </div>
-                            <div>
-                                <p style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 15, marginBottom: 30, maxWidth: '450px'}}>
-                                    Sample Text Sample Text Sample Text Sample TextSample Text
-                                    Sample Text Sample Text Sample Text Sample Text Sample Text
-                                    Sample Text Sample Text Sample Text Sample Text  
-                                </p>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+      <div>
+        <div style={{ marginLeft: "350px", marginTop: "50px" }}>
+          <ul style={{ display: "flex" }}>
+            <li>
+              <div style={{font: 'Times-Roman', color: '#EB3B15', fontWeight: 'bolder', fontSize: 30}}>
+                  LATEST NEWS
+              </div>
+              <div>
+                {articles.map((article) => {
+                  return (
+                    <NewsItem
+                      title={article.title}
+                      description={article.description}
+                      url={article.url}
+                      urlToImage={article.urlToImage}
+                    ></NewsItem>
+                  );
+                })}
+              </div>
+            </li>
+            <li>
+              <div style={{ marginLeft: "30px" }}>
+                <div style={{ marginBottom: "10px" }}>
+                  <h2
+                    style={{
+                      font: "Times-Roman",
+                      color: "#EB3B15",
+                      fontWeight: "bolder",
+                      fontSize: 30,
+                    }}
+                  >
+                    POPULAR NEWS
+                  </h2>
+                </div>
+                <div>
+                  <p
+                    style={{
+                      font: "Times-Roman",
+                      fontWeight: "bolder",
+                      fontSize: 15,
+                      marginBottom: 30,
+                      maxWidth: "450px",
+                    }}
+                  >
+                    Sample Text Sample Text Sample Text Sample TextSample Text
+                    Sample Text Sample Text Sample Text Sample Text Sample Text
+                    Sample Text Sample Text Sample Text Sample Text
+                  </p>
+                </div>
+                <div>
+                  <p
+                    style={{
+                      font: "Times-Roman",
+                      fontWeight: "bolder",
+                      fontSize: 15,
+                      marginBottom: 30,
+                      maxWidth: "450px",
+                    }}
+                  >
+                    Sample Text Sample Text Sample Text Sample TextSample Text
+                    Sample Text Sample Text Sample Text Sample Text Sample Text
+                    Sample Text Sample Text Sample Text Sample Text
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsFeed
+export default NewsFeed;
---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NewsItem = ({title, description, url, urlToImage} : {title : string, description : string, url : string, urlToImage : string}) => {
+  return (
+    <div>
+        <div style={{maxWidth: '650px', display: 'flex', marginTop: '30px'}}>
+            <img className='news-img' style={{height: '200px', width: '400px'}} src={urlToImage}/>
+            <div style={{marginLeft: '20px'}}>
+                <h1 style={{font: 'Times-Roman', fontWeight: 'bolder', fontSize: 30, display: 'grid'}}>
+                    <a href={url}>{title}</a>
+                </h1>
+                <p>
+                    {description}
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NewsItem
